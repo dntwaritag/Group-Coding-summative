7a12bfede1c1f90920dd6459213b1f97c4f0e873
Denys NTWARITAGANZWA <d.ntwaritag@alustudent.com>
2023-06-24 03:26:42 -0700

    move-to-directory.sh

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -2,10 +2,10 @@
 
 # Create the directory name based on the negpod_id and q1
 directory_name="0323-RW-SOFEN-8120120 11-q1"
-mkdir "$0323-RW-SOFEN-8120120 11-q1"
+mkdir "$directory_name"
 
 # Move the files created in question 1 to the directory
-mv students-list_0333.txt main.sh select-emails.sh "$0323-RW-SOFEN-8120120 11-q1"
+mv students-list_0333.txt main.sh select-emails.sh "$directory_name"
 
-echo "Files moved to $0323-RW-SOFEN-8120120 11-q1."
+echo "Files moved to $directory_name."
 
